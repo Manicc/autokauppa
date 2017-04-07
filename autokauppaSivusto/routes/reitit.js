dddce427ab15df047f59cb2b015fb92d029d483c
Manicc <mapekkala@gmail.com>
2017-04-07 12:59:30 +0300

    Tietokantaan lisäys
    /lisaa sivu lisätty

--- autokauppaSivusto/routes/reitit.js
+++ autokauppaSivusto/routes/reitit.js
@@ -41,6 +41,10 @@ router.get('/auto', function(req, res, next) {
   res.render('auto');
 });
 
+router.get('/lisaa', function(req, res, next) {
+  res.render('lisaa');
+});
+
 
 
 
@@ -48,6 +52,33 @@ router.post('/hakuprosessi',function(req,res){
 	console.log('Form (from querystring): ' + req.query.form);
 	console.log('Name (from visible form field): ' + req.body.name);
 	console.log('Email (from visible form field): ' + req.body.email);
+	res.redirect(303,'/');
+});
+router.post('/lisays-prosessointi',function(req,res){
+	
+		new Auto({
+		id: req.body.id,
+		merkki: req.body.merkki,
+		malli: req.body.malli,
+		korimalli: req.body.korimalli,
+		polttoaine: req.body.polttoaine,
+		vaihteisto: req.body.vaihteisto,
+		vari: req.body.vari,
+		vuosi: req.body.vuosi,
+		hinta: req.body.hinta,
+		}).save();
+		
+	console.log('Form (from querystring): ' + req.query.form);
+	console.log(req.body.id);
+	console.log(req.body.merkki);
+	console.log(req.body.malli);
+	console.log(req.body.korimalli);
+	console.log(req.body.polttoaine);
+	console.log(req.body.vaihteisto);
+	console.log(req.body.vari);
+	console.log(req.body.vuosi);
+	console.log(req.body.hinta);
+	
 	res.redirect(303,'/');
 });
 
