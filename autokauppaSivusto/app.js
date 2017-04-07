114744cd533b5b492b4ef750e904aebea31a0227
Manicc <mapekkala@gmail.com>
2017-04-07 10:48:01 +0300

    Homma pelittää

--- autokauppaSivusto/app.js
+++ autokauppaSivusto/app.js
@@ -1,46 +1,53 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/reitit');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var hbs = require('express-handlebars')
+	.create({defaultLayout: 'main',
+	extname: '.hbs',
+	helpers: {
+		section: function(name, options){
+			if(!this.sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	}
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+// view engine setup
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('port', process.env.PORT || 3000);
+
+
+
+
+// Static middleware
+
+// Kaikki reitit tänne!
+app.use('/', index);
+
+app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser').urlencoded({extended:true}));
+
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(req,res){
+	console.error(err.stack);
+	res.status(500);
+	render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express käynnistetty osoitteeseen http://localhost:' + app.get('port') + '; Sulje palvelin Ctrl-c');	
 });
 
 module.exports = app;
